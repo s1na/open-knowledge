745106fd6b86da02f212ae4efc134d06c8c7f297
Sina Mahmoodi <itz.s1na@gmail.com>
2018-05-07 12:40:10 +0200

    Add initial project structure
    Signed-off-by: Sina Mahmoodi <itz.s1na@gmail.com>

---
+++ example/index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const IPFS = require('ipfs')
+const OrbitDB = require('orbit-db')
+const OpenKnowledge = require('../dist/open-knowledge.cjs.js')
+
+const ipfsOptions = {
+  EXPERIMENTAL: {
+    pubsub: true
+  }
+}
+
+const ipfs = new IPFS(ipfsOptions)
+
+ipfs.on('error', (e) => console.error(e))
+ipfs.on('ready', async () => {
+  const orbit = new OrbitDB(ipfs)
+  const ok = new OpenKnowledge(orbit)
+})
---
+++ src/index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+export default class OpenKnowledge {
+  constructor(orbitdb) {
+    this.orbitdb = orbitdb 
+  }
+}
