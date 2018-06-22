4a7331ff18500ffda8545be5cbb616b7c72ee38a
Sina Mahmoodi <itz.s1na@gmail.com>
2018-06-22 21:12:33 +0200

    Fix rename
    Signed-off-by: Sina Mahmoodi <itz.s1na@gmail.com>

--- test/graph-manager.js
+++ test/graph-manager.js
@@ -1,37 +1,37 @@
 const GraphRegistry = artifacts.require('GraphRegistry')
-const GraphManager = artifacts.require('GraphManager')
+const Graph = artifacts.require('Graph')
 
-contract('GraphManager', async (accounts) => {
+contract('Graph', async (accounts) => {
   let registry
-  let defaultGM
+  let defaultG
 
   before(async () => {
     registry = await GraphRegistry.deployed()
     let defaultAddr = await registry.graphs.call('default')
-    defaultGM = await GraphManager.at(defaultAddr)
+    defaultG = await Graph.at(defaultAddr)
   })
 
   it('should have msg.sender as owner', async () => {
-    let owner = await defaultGM.owner.call()
+    let owner = await defaultG.owner.call()
     assert.equal(owner, accounts[0])
   })
 
   it('should have default root', async () => {
-    let root = await defaultGM.root.call()
+    let root = await defaultG.root.call()
     assert.equal(root, '0x017112200d511ee9a3ab4e52e8e2bc40fd2669d9c44b89164107e9898cd9698c1506c5aa')
   })
 
   it('should set root correctly', async () => {
     let hex = '0x01711220c19a797fa1fd590cd2e5b42d1cf5f246e29b91684e2f87404b81dc345c7a56a0'
-    let tx = await defaultGM.setRoot(hex)
-    let root = await defaultGM.root.call()
+    let tx = await defaultG.setRoot(hex)
+    let root = await defaultG.root.call()
     assert.equal(root, hex)
   })
 
   it('should check owner when setting root', async () => {
     let hex = '0x017112200d511ee9a3ab4e52e8e2bc40fd2669d9c44b89164107e9898cd9698c1506c5aa'
     try {
-      let tx = await defaultGM.setRoot(hex, { from: accounts[1] })
+      let tx = await defaultG.setRoot(hex, { from: accounts[1] })
       assert.fail('Expected revert not received')
     } catch (e) {
       const revertFound = e.message.search('revert') >= 0
