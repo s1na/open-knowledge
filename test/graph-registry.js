556b42dee3803624070a5c2533e519bc0328dd3f
Sina Mahmoodi <itz.s1na@gmail.com>
2018-07-01 11:31:22 +0200

    Test: add cases for getGraphsCount and getGraphName
    Signed-off-by: Sina Mahmoodi <itz.s1na@gmail.com>

--- test/graph-registry.js
+++ test/graph-registry.js
@@ -1,23 +1,46 @@
 const GraphRegistry = artifacts.require('GraphRegistry')
+const Web3 = require('web3')
+const web3 = new Web3()
 
 contract('GraphRegistry', async (accounts) => {
+  let inst
+
+  before(async () => {
+    inst = await GraphRegistry.deployed()
+  })
+
+  it('should contain one graph', async () => {
+    let c = await inst.getGraphsCount()
+    assert.equal(c, 1)
+  })
+
   it('should create default graph', async () => {
-    let inst = await GraphRegistry.deployed()
     let defaultG = await inst.graphs.call('default')
     assert.notEqual(defaultG, '0x0000000000000000000000000000000000000000')
   })
 
+  it('should have default graph in index 0', async () => {
+    let hex = await inst.getGraphName(0)
+    let name = web3.utils.hexToUtf8(hex)
+    assert.equal(name, 'default')
+  })
+
   it('should create new graph', async () => {
-    let inst = await GraphRegistry.deployed()
     let tx = await inst.newGraph('test', { from: accounts[0] })
     let addr = tx.logs[0].args.addr
 
     let res = await inst.graphs.call('test')
     assert.equal(addr, res)
+
+    let c = await inst.getGraphsCount()
+    assert.equal(c, 2)
+
+    let hex = await inst.getGraphName(1)
+    let name = web3.utils.hexToUtf8(hex)
+    assert.equal(name, 'test')
   })
 
   it('shouldnt overwrite existing', async () => {
-    let inst = await GraphRegistry.deployed()
     try {
       let tx = await inst.newGraph('test', { from: accounts[1] })
       assert.fail('Expected revert not received')
