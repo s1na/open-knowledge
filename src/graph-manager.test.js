d174bf20e6eaea9243a7c45f1fc22ff92965a988
Sina Mahmoodi <itz.s1na@gmail.com>
2018-07-03 14:22:47 +0200

    Test: add tests for graph-manager.js
    Signed-off-by: Sina Mahmoodi <itz.s1na@gmail.com>

---
+++ src/graph-manager.test.js
@@ -0,0 +1,44 @@
+import Ganache from 'ganache-core'
+import Web3 from 'web3'
+
+import GraphManager from './graph-manager'
+import Graph from './graph'
+import GraphContract from '../build/contracts/Graph.json'
+import ipfs from './ipfs-mem'
+
+let graph
+let m
+
+beforeAll(async () => {
+  await ipfs.dag.init()
+
+  const provider = Ganache.provider({})
+  const web3 = new Web3(provider)
+  let accounts = await web3.eth.getAccounts()
+  let graphContract = new web3.eth.Contract(GraphContract.abi)
+  let gas = await graphContract.deploy({ data: GraphContract.bytecode, arguments: [accounts[0]] }).estimateGas({ from: accounts[0] })
+  let contract = await graphContract.deploy({ data: GraphContract.bytecode, arguments: [accounts[0]] }).send({ from: accounts[0], gas })
+  graph = new Graph(web3, contract)
+  m = new GraphManager(ipfs, graph)
+})
+
+test('should instantiate', async () => {
+  expect(typeof m).toBe('object')
+
+  await m.init()
+  expect(m.store).toBeDefined()
+})
+
+test('should add triples', async () => {
+  let tx = await m.addTriples([['http://example.com/s', 'http://example.com/p', 'http://example.com/o']])
+  expect(tx === null).toBe(false)
+  expect(tx).toHaveProperty('events.RootUpdated.returnValues.root')
+  expect(await graph.root()).toEqual(await m.store.root)
+})
+
+test('should execute query', async () => {
+  let q = 'PREFIX : <http://example.com/> SELECT * WHERE { ?s :p :o }'
+  let res = await m.execute(q)
+  expect(res).toHaveLength(1)
+  expect(res[0]).toEqual({ '?s': 'http://example.com/s' })
+})
