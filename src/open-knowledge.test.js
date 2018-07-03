0ea59922c00bc1063a6f939554cd4f3694a29de7
Sina Mahmoodi <itz.s1na@gmail.com>
2018-07-03 18:04:01 +0200

    Test: add tests for open-knowledge.js and an integration test
    Signed-off-by: Sina Mahmoodi <itz.s1na@gmail.com>

--- src/open-knowledge.test.js
+++ src/open-knowledge.test.js
@@ -1,6 +1,46 @@
-import OpenKnowledge from './open-knowledge'
+import Ganache from 'ganache-core'
+import Web3 from 'web3'
 
-test('instantiates an object', () => {
-  let ok = new OpenKnowledge(null, null)
-  expect(ok.ipfs).toBeNull()
+import OpenKnowledge from './open-knowledge'
+import GraphRegistry from './graph-registry'
+import GraphRegistryContract from '../build/contracts/GraphRegistry.json'
+import ipfs from './ipfs-mem'
+
+let registry
+let ok
+
+beforeAll(async () => {
+  await ipfs.dag.init()
+
+  const provider = Ganache.provider({})
+  const web3 = new Web3(provider)
+  let accounts = await web3.eth.getAccounts()
+  let graphRegistryContract = new web3.eth.Contract(GraphRegistryContract.abi)
+  let gas = await graphRegistryContract.deploy({ data: GraphRegistryContract.bytecode }).estimateGas({ from: accounts[0] })
+  let contract = await graphRegistryContract.deploy({ data: GraphRegistryContract.bytecode }).send({ from: accounts[0], gas })
+  registry = new GraphRegistry(web3, contract)
+  ok = new OpenKnowledge(ipfs, registry)
+  await ok.init()
+})
+
+test('should instantiate', async () => {
+  expect(typeof ok).toBe('object')
+})
+
+test('should have default graph manager', async () => {
+  let m = await ok.getGraphManager('default')
+  expect(typeof m).toBe('object')
+
+  let managers = await ok.getGraphs()
+  expect(managers).toHaveProperty('default')
+  expect(managers.default).toHaveProperty('graph')
+})
+
+test('should add new graph manager', async () => {
+  let m = await ok.newGraphManager('test')
+  expect(typeof m).toBe('object')
+  expect(m).toHaveProperty('graph')
+
+  let managers = await ok.getGraphs()
+  expect(managers).toHaveProperty('test')
 })
