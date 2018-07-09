cba13d8881cabbde1a23d1d21add52fb5eae0c1b
Sina Mahmoodi <itz.s1na@gmail.com>
2018-07-09 13:20:10 +0200

    Fix variable detection in fragments client
    Signed-off-by: Sina Mahmoodi <itz.s1na@gmail.com>

--- src/fragments-client.js
+++ src/fragments-client.js
@@ -9,11 +9,11 @@ export default class FragmentsClient {
 
   getFragmentByPattern (pattern) {
     let fragment = new Fragment()
-    let s = pattern.subject === '?s' ? null : pattern.subject
-    let p = pattern.predicate === '?p' ? null : pattern.predicate
-    let o = pattern.object === '?o' ? null : pattern.object
+    let s = pattern.subject[0] === '?' ? null : pattern.subject
+    let p = pattern.predicate[0] === '?' ? null : pattern.predicate
+    let o = pattern.object[0] === '?' ? null : pattern.object
 
-    this.store.getTriples(s, p, o, 'test').then((res) => {
+    this.store.getTriples(s, p, o).then((res) => {
       fragment.setProperty('metadata', { totalTriples: res.length })
       for (let i = 0; i < res.length; i++) {
         let triple = { subject: res[i][0], predicate: res[i][1], object: res[i][2] }
